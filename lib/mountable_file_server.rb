4ab8c08aa912795860e7838200e1e36d38ecdd61
David Strauß <david@strauss.io>
2015-06-12 22:58:33 +0200

    Move Ruby on Rails specific stuff into it's own file

--- lib/mountable_file_server.rb
+++ lib/mountable_file_server.rb
@@ -5,9 +5,4 @@ module MountableFileServer
   require "mountable_file_server/upload"
   require "mountable_file_server/storage"
   require "mountable_file_server/endpoint"
-
-  # require "mountable_file_server/backend"
-  require "mountable_file_server/engine"
-  # require "mountable_file_server/access_helper"
-  # require "mountable_file_server/storage_helper"
  end
---
+++ lib/mountable_file_server/rails.rb
@@ -0,0 +1,6 @@
+require 'mountable_file_server'
+
+module MountableFileServer
+  class Engine < Rails::Engine
+  end
+end
