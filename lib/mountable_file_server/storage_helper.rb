9df24ca2121ec14ece0621cf3c25fbf88f884d4f
David Strauß <david@strauss.io>
2015-06-12 21:25:49 +0200

    Remove legacy code files

--- lib/mountable_file_server/storage_helper.rb
+++
@@ -1,9 +0,0 @@
-module MountableFileServer
-  module StorageHelper
-    def move_upload_to_storage(filename)
-      from = File.join(MountableFileServer.configuration.root, MountableFileServer.configuration.stored_at, 'tmp', filename)
-      to = File.join(MountableFileServer.configuration.root, MountableFileServer.configuration.stored_at, 'public', filename)
-      FileUtils.move from, to
-    end
-  end
-end
