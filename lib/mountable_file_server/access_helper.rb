bce630ad43e815153344a55a98720141e6e097a2
David Strauß <david@strauss.io>
2015-08-13 09:08:59 +0200

    Remove unused files from prototype

--- lib/mountable_file_server/access_helper.rb
+++
@@ -1,7 +0,0 @@
-# module MountableFileServer
-#   module AccessHelper
-#     def path_for_upload(filename)
-#       File.join(MountableFileServer.configuration.mounted_at, filename)
-#     end
-#   end
-# end
