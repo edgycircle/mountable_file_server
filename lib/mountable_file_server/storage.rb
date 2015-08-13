d00b451f997643ea332e82e595d7a93e0f3374f7
David Strauß <david@strauss.io>
2015-08-13 20:58:29 +0200

    Adapt storage to use persistor

--- lib/mountable_file_server/storage.rb
+++ lib/mountable_file_server/storage.rb
@@ -1,52 +1,36 @@
 module MountableFileServer
   class Storage
-    attr_reader :access
+    attr_reader :persistor_class, :configuration
 
-    def initialize(configuration = MountableFileServer.configuration)
-      @access = Access.new configuration
+    def initialize(persistor_class = Persistor, configuration = MountableFileServer.configuration)
+      @configuration = configuration
+      @persistor_class = persistor_class
     end
 
-    def store_temporary(path:, type:, filename:)
-      file_to_be_stored = File.new path
-      identifier = random_identifier filename: filename, type: type
-      path = access.temporary_path_for identifier: identifier
-
-      File.open(path, 'wb') do |file|
-        file.write file_to_be_stored.read
-      end
-
-      identifier
+    def store_temporary(io, identifier)
+      pathname = FileAccessor.new(identifier, configuration).temporary_pathname
+      persistor = persistor_class.new pathname
+      persistor.save io
     end
 
-    def store_permanently(path:, type:, filename:)
-      identifier = store_temporary path: path, type: type, filename: filename
-      move_to_permanent_storage identifier: identifier
-      identifier
+    def store_permanent(io, identifier)
+      pathname = FileAccessor.new(identifier, configuration).permanent_pathname
+      persistor = persistor_class.new pathname
+      persistor.save io
     end
 
-    def move_to_permanent_storage(identifier:)
-      identifier = Identifier.new identifier
-      from = access.temporary_path_for identifier: identifier
-      to = access.path_for identifier: identifier
-
-      FileUtils.move from, to
+    def move_to_permanent_storage(identifier)
+      accessor = FileAccessor.new(identifier, configuration)
+      temporary_pathname = accessor.temporary_pathname
+      permanent_pathname = accessor.permanent_pathname
+      persistor = persistor_class.new temporary_pathname
+      persistor.rename permanent_pathname
     end
 
-    def remove_from_permanent_storage(identifier:)
-      identifier = Identifier.new identifier
-      path = access.path_for identifier: identifier
-
-      FileUtils.rm path
-    end
-
-  private
-    def random_identifier(filename:, type:)
-      loop do
-        identifier = Identifier.generate_for filename: filename, type: type
-        path = access.temporary_path_for identifier: identifier
-
-        break identifier unless File.exists?(path)
-      end
+    def remove_from_permanent_storage(identifier)
+      pathname = FileAccessor.new(identifier, configuration).permanent_pathname
+      persistor = persistor_class.new pathname
+      persistor.delete
     end
   end
 end
--- test/unit/storage_test.rb
+++ test/unit/storage_test.rb
@@ -1,109 +1,64 @@
 require 'unit_helper'
+require 'stringio'
+require 'tempfile'
 
 class StorageTest < UnitTestCase
-  def setup
-    FileUtils.mkdir_p tmp_path
-    FileUtils.mkdir_p public_path
-    FileUtils.mkdir_p private_path
-  end
-
-  def teardown
-    FileUtils.rm_rf tmp_path
-    FileUtils.rm_rf public_path
-    FileUtils.rm_rf private_path
-  end
-
   def test_store_temporary
-    type = 'public'
-    path = fixture_path('david.jpg')
-    identifier = storage.store_temporary path: path, type: 'public', filename: 'david.jpg'
+    stream = StringIO.new 'test'
+    id = MountableFileServer::Identifier.new 'public-test.txt'
+    pathname = MountableFileServer::FileAccessor.new(id).temporary_pathname
 
-    assert File.exist?(File.join(tmp_path, identifier))
+    persistor = MiniTest::Mock.new
+    persistor.expect :new, persistor, [pathname]
+    persistor.expect :save, nil, [stream]
+
+    storage = MountableFileServer::Storage.new persistor
+    storage.store_temporary stream, id
+
+    assert persistor.verify
   end
 
-  def test_store_permanently
-    type = 'public'
-    path = fixture_path('david.jpg')
-    identifier = storage.store_permanently path: path, type: 'public', filename: 'david.jpg'
+  def test_store_permanent
+    stream = StringIO.new 'test'
+    id = MountableFileServer::Identifier.new 'public-test.txt'
+    pathname = MountableFileServer::FileAccessor.new(id).permanent_pathname
 
-    assert File.exist?(File.join(public_path, identifier.filename))
+    persistor = MiniTest::Mock.new
+    persistor.expect :new, persistor, [pathname]
+    persistor.expect :save, nil, [stream]
+
+    storage = MountableFileServer::Storage.new persistor
+    storage.store_permanent stream, id
+
+    assert persistor.verify
   end
 
-  def test_identifier_is_unique_within_temporary_directory
-    type = 'public'
-    path = fixture_path('david.jpg')
-    random_tokens = ['a', 'a', 'b']
-    stub(SecureRandom).hex { random_tokens.shift }
+  def test_remove_from_permanent_storage
+    id = MountableFileServer::Identifier.new 'public-test.txt'
+    pathname = MountableFileServer::FileAccessor.new(id).permanent_pathname
 
-    identifier_one = storage.store_temporary path: path, type: 'public', filename: 'david.jpg'
-    identifier_two = storage.store_temporary path: path, type: 'public', filename: 'david.jpg'
+    persistor = MiniTest::Mock.new
+    persistor.expect :new, persistor, [pathname]
+    persistor.expect :delete, nil, []
 
-    assert_equal 'public-a.jpg', identifier_one
-    assert_equal 'public-b.jpg', identifier_two
+    storage = MountableFileServer::Storage.new persistor
+    storage.remove_from_permanent_storage id
+
+    assert persistor.verify
   end
 
-  def test_permanent_storage
-    path = fixture_path('david.jpg')
+  def test_move_to_permanent_storage
+    id = MountableFileServer::Identifier.new 'public-test.txt'
+    pathname_a = MountableFileServer::FileAccessor.new(id).temporary_pathname
+    pathname_b = MountableFileServer::FileAccessor.new(id).permanent_pathname
 
-    public_identifier = storage.store_temporary path: path, type: 'public', filename: 'david.jpg'
-    private_identifier = storage.store_temporary path: path, type: 'private', filename: 'david.jpg'
+    persistor = MiniTest::Mock.new
+    persistor.expect :new, persistor, [pathname_a]
+    persistor.expect :rename, nil, [pathname_b]
 
-    storage.move_to_permanent_storage identifier: public_identifier
-    storage.move_to_permanent_storage identifier: private_identifier
+    storage = MountableFileServer::Storage.new persistor
+    storage.move_to_permanent_storage id
 
-    refute File.exists? File.join(tmp_path, public_identifier)
-    refute File.exists? File.join(tmp_path, private_identifier)
-    refute File.exists? File.join(private_path, public_identifier)
-    refute File.exists? File.join(private_path, private_identifier)
-    refute File.exists? File.join(public_path, public_identifier)
-    refute File.exists? File.join(public_path, private_identifier)
-    refute File.exists? File.join(private_path, public_identifier.filename)
-    assert File.exists? File.join(private_path, private_identifier.filename)
-    assert File.exists? File.join(public_path, public_identifier.filename)
-    refute File.exists? File.join(public_path, private_identifier.filename)
-  end
-
-  def test_removing_identifiers
-    path = fixture_path('david.jpg')
-
-    public_identifier = storage.store_temporary path: path, type: 'public', filename: 'david.jpg'
-    private_identifier = storage.store_temporary path: path, type: 'private', filename: 'david.jpg'
-
-    storage.move_to_permanent_storage identifier: public_identifier
-    storage.move_to_permanent_storage identifier: private_identifier
-
-    storage.remove_from_permanent_storage identifier: public_identifier
-    storage.remove_from_permanent_storage identifier: private_identifier
-
-    refute File.exists? File.join(private_path, private_identifier.filename)
-    refute File.exists? File.join(public_path, public_identifier.filename)
-  end
-
-private
-  def tmp_path
-    File.expand_path('../../tmp/test-uploads/tmp', File.dirname(__FILE__))
-  end
-
-  def public_path
-    File.expand_path('../../tmp/test-uploads/public', File.dirname(__FILE__))
-  end
-
-  def private_path
-    File.expand_path('../../tmp/test-uploads/private', File.dirname(__FILE__))
-  end
-
-  def fake_upload_parameters
-    {
-      filename: 'david.jpg',
-      tempfile: File.new(fixture_path('david.jpg'))
-    }
-  end
-
-  def configuration
-    MountableFileServer::Configuration.new mounted_at: '/uploads', stored_at: File.join(tmp_path, '../')
-  end
-
-  def storage
-    MountableFileServer::Storage.new configuration
+    assert persistor.verify
   end
 end
