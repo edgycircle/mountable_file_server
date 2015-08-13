3ab3dd0e6dae44b5983d7885eb751c47f5bb1003
David Strauß <david@strauss.io>
2015-08-13 19:35:14 +0200

    Refactor persistor

--- lib/mountable_file_server/persistor.rb
+++ lib/mountable_file_server/persistor.rb
@@ -2,13 +2,25 @@ require 'pathname'
 
 module MountableFileServer
   class Persistor
-    def initialize(destination)
-      @destination = destination
+    attr_reader :pathname
+
+    def initialize(pathname)
+      @pathname = Pathname(pathname)
     end
 
     def save(source)
-      Pathname.new(@destination).dirname.mkpath
-      IO.copy_stream source, @destination
+      pathname.dirname.mkpath
+      IO.copy_stream source, pathname
+    end
+
+    def delete
+      pathname.delete
+    end
+
+    def rename(new_pathname)
+      persistor = Persistor.new new_pathname
+      persistor.save pathname
+      delete
     end
   end
 end
--- test/unit/persistor_test.rb
+++ test/unit/persistor_test.rb
@@ -26,17 +26,41 @@ class PersistorTest < UnitTestCase
   end
 
   def test_save_pathname_to_pathname
-    source = Tempfile.new('test')
-    source.write 'test'
-    source.close
+    io = Tempfile.new('test')
+    io.write 'test'
+    io.close
 
     Tempfile.open('test') do |file|
       persistor = MountableFileServer::Persistor.new file.path
-      persistor.save source.path
+      persistor.save io.path
 
       assert_equal 'test', file.read
     end
 
-    source.unlink
+    io.unlink
+  end
+
+  def test_delete_pathname
+    file = Tempfile.new 'test'
+
+    persistor = MountableFileServer::Persistor.new file.path
+    persistor.delete
+
+    refute Pathname(file.path).exist?
+  end
+
+  def test_rename_pathname
+    file = Tempfile.new 'test'
+    file.write 'test'
+    file.close
+
+    new_pathname = Pathname(file.path).dirname + 'test-2'
+
+    persistor = MountableFileServer::Persistor.new file.path
+    persistor.rename new_pathname
+
+    refute Pathname(file.path).exist?
+    assert new_pathname.exist?
+    assert_equal 'test', new_pathname.read
   end
 end
