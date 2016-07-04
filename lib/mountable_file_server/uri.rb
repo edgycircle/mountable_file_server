f125ce273919dc7e1d09fea081b34240767bde37
David Strauß <david@strauss.io>
2016-07-04 13:03:15 +0200

    Remove obsolete files and code

--- lib/mountable_file_server/uri.rb
+++ lib/mountable_file_server/uri.rb
@@ -1,6 +1,5 @@
 require 'uri'
 require 'pathname'
-require 'url_safe_base64'
 
 module MountableFileServer
   class URI < String
@@ -12,22 +11,8 @@ module MountableFileServer
       UniqueIdentifier.new filename
     end
 
-    def encoded_processing_instructions
-      /\.(\w+)\./.match(filename).captures.first
-    end
-
-    def decoded_processing_instructions
-      UrlSafeBase64.decode64 encoded_processing_instructions
-    end
-
     def filename
       Pathname(::URI.parse(self).path).basename.to_s
-    end
-
-    def resize(instructions)
-      encoded_instructions = UrlSafeBase64.encode64 instructions
-
-      URI.new self.gsub('.', ".#{encoded_instructions}.")
     end
   end
 end
--- test/unit/uri_test.rb
+++ test/unit/uri_test.rb
@@ -15,22 +15,4 @@ class URITest < UnitTestCase
     assert_instance_of UniqueIdentifier, uri.uid
     assert_equal 'public-test.jpg', uri.uid
   end
-
-  def test_encoded_processing_instructions
-    uri = URI.new '/uploads/public-test.YWJjREVG.jpg'
-    assert_equal 'YWJjREVG', uri.encoded_processing_instructions
-  end
-
-  def test_decoded_processing_instructions
-    uri = URI.new '/uploads/public-test.YWJjREVG.jpg'
-    assert_equal 'abcDEF', uri.decoded_processing_instructions
-  end
-
-  def test_generate_new_url_with_processing_instructions
-    uri = URI.new '/uploads/public-test.jpg'
-    resized_uri = uri.resize 'abcDEF'
-
-    assert_instance_of URI, resized_uri
-    assert_equal '/uploads/public-test.YWJjREVG.jpg', resized_uri
-  end
 end
