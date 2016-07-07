50859e884d24ae0a5cc781387f03608f548e981a
David Strauß <david@strauss.io>
2016-07-07 12:56:16 +0200

    Add indentation

--- lib/mountable_file_server/client.rb
+++ lib/mountable_file_server/client.rb
@@ -6,34 +6,34 @@ module MountableFileServer
   class Client
     def move_to_permanent_storage(fid)
       if MountableFileServer.config.base_url.start_with?('http')
-      uri = ::URI.parse(MountableFileServer.config.base_url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.set_debug_output($stdout)
-      request = Net::HTTP::Post.new(uri.request_uri + fid + '/store-permanent')
+        uri = ::URI.parse(MountableFileServer.config.base_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.set_debug_output($stdout)
+        request = Net::HTTP::Post.new(uri.request_uri + fid + '/store-permanent')
 
-      http.request(request)
+        http.request(request)
       else
-      MountableFileServer::Server.new.call({
-        "rack.input" => StringIO.new(""),
-        "REQUEST_METHOD"=> "POST",
-        "PATH_INFO"=> "/#{fid}/store-permanent",
-      })
+        MountableFileServer::Server.new.call({
+          "rack.input" => StringIO.new(""),
+          "REQUEST_METHOD"=> "POST",
+          "PATH_INFO"=> "/#{fid}/store-permanent",
+        })
       end
     end
 
     def remove_from_storage(fid)
       if MountableFileServer.config.base_url.start_with?('http')
-      uri = ::URI.parse(MountableFileServer.config.base_url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Delete.new(uri.request_uri + fid)
+        uri = ::URI.parse(MountableFileServer.config.base_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Delete.new(uri.request_uri + fid)
 
-      http.request(request)
+        http.request(request)
       else
-      MountableFileServer::Server.new.call({
-        "rack.input" => StringIO.new(""),
-        "REQUEST_METHOD"=> "DELETE",
-        "PATH_INFO"=> "/#{fid}",
-      })
+        MountableFileServer::Server.new.call({
+          "rack.input" => StringIO.new(""),
+          "REQUEST_METHOD"=> "DELETE",
+          "PATH_INFO"=> "/#{fid}",
+        })
       end
     end
 
